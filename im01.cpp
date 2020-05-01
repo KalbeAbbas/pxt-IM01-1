10a6bf69c05fccab99dac0216e62032a37031b7b
KalbeAbbas <kalbeabbas142@gmail.com>
2020-05-01 17:29:09 +0500

    testing SD card reading function

--- im01.cpp
+++ im01.cpp
@@ -63,7 +63,7 @@ String _read(String s)
         return mkString(cant_read_file, strlen(cant_read_file));
 	}
 	
-	String str = mkString(_word, strlen(_word));
+	String str = mkString(path, strlen(path));
 	
 	free(_word);
 	return str;
